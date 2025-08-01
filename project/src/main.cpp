295183c813fb66de536cad2ede2e5153beeabfbe
minerkid08 <xand3rc0lby@gmail.com>
2025-08-01 14:24:34 -0400

    added platform things

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,8 +1,31 @@
 #include "export/project.hpp"
 #include "export/settings.hpp"
+#include "json/json.hpp"
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+
+#ifdef __linux
+#define OSSTR "linux"
+#endif
+
+#ifdef _WIN64
+#define OSSTR "windows"
+#endif
+
+#define getField(name, res)                                                                                            \
+	if (osJson.contains(name))                                                                                         \
+		res = osJson[name];                                                                                            \
+	else                                                                                                               \
+		res = json[name];
+
+#define tryGetField(name, res)                                                                                         \
+	if (osJson.contains(name))                                                                                         \
+		res = osJson[name];                                                                                            \
+	else if (json.contains(name))                                                                                      \
+		res = json[name];
+
+#define hasField(name) osJson.contains(name) | json.contains(name)
 
 void parseSettings(Settings* settings, const nlohmann::json& json)
 {
@@ -61,7 +84,11 @@ void findProject(const char* name, const JsonObjects& objects, Project* project)
 			else
 				json = j;
 			project->path = objects.libPath / str;
-			parseProject(json, project);
+
+			if (json.contains(OSSTR))
+				parseProject(json[OSSTR], project);
+			else
+				parseProject(json, project);
 			return;
 		}
 	}
@@ -71,11 +98,15 @@ void findProject(const char* name, const JsonObjects& objects, Project* project)
 
 void parseProject(const nlohmann::json& json, Project* p)
 {
-	p->name = json["name"];
-	if (json.contains("out"))
-		p->out = json["out"];
+	nlohmann::json osJson;
+	if (json.contains(OSSTR))
+		osJson = json[OSSTR];
 
-	const std::string& type = json["type"];
+	getField("name", p->name);
+	tryGetField("out", p->out);
+
+	std::string type;
+	getField("type", type);
 	if (type == "header")
 		p->type = ProjectType::Header;
 	else if (type == "program")
@@ -85,47 +116,60 @@ void parseProject(const nlohmann::json& json, Project* p)
 	else if (type == "shared")
 		p->type = ProjectType::Shared;
 
-	if (json.contains("build"))
+	if (hasField("build"))
 	{
-		for (const std::string& str : json["build"])
+		nlohmann::json build;
+		getField("build", build);
+		for (const std::string& str : build)
 			p->build.push_back(p->path / str);
 		p->build.push_back(p->path / "src");
 		p->include.push_back(p->path / "src");
 	}
 
-	if (json.contains("include"))
+	if (hasField("include"))
 	{
-		for (const std::string& str : json["include"])
+		nlohmann::json include;
+		getField("include", include);
+		for (const std::string& str : include)
 			p->include.push_back(p->path / str);
 	}
 
-	if (json.contains("export"))
+	if (hasField("export"))
 	{
-		for (const std::string& str : json["export"])
+		nlohmann::json export2;
+		getField("export", export2);
+		for (const std::string& str : export2)
 			p->exportPaths.push_back(p->path / str);
 	}
 
-	if (json.contains("libs"))
+	if (hasField("libs"))
 	{
-		for (const std::string& str : json["libs"])
+		nlohmann::json libs;
+		getField("libs", libs);
+		for (const std::string& str : libs)
 			p->libs.push_back(str);
 	}
 
-	if (json.contains("compArgs"))
+	if (hasField("compArgs"))
 	{
-		for (const std::string& str : json["compArgs"])
+		nlohmann::json compArgs;
+		getField("compArgs", compArgs);
+		for (const std::string& str : compArgs)
 			p->compArgs.push_back(str);
 	}
 
-	if (json.contains("linkArgs"))
+	if (hasField("linkArgs"))
 	{
-		for (const std::string& str : json["linkArgs"])
+		nlohmann::json linkArgs;
+		getField("linkArgs", linkArgs);
+		for (const std::string& str : linkArgs)
 			p->linkArgs.push_back(str);
 	}
 
-	if (json.contains("lang"))
+	if (hasField("lang"))
 	{
-		std::string lang = json["lang"];
+		std::string lang;
+		getField("lang", lang);
 		if (lang == "cpp")
 			p->lang = ProjectLang::CPP;
 		else
@@ -136,16 +180,21 @@ void parseProject(const nlohmann::json& json, Project* p)
 		p->lang = ProjectLang::CPP;
 	}
 
+	if (osJson.contains("targetArgs"))
+	{
+		const nlohmann::json& targetArgs = osJson["targetArgs"];
+		for (const auto& args : targetArgs.items())
+			p->configArgs[args.key()] = args.value();
+	}
 	if (json.contains("targetArgs"))
 	{
-		for (const auto& args : json["targetArgs"].items())
+		const nlohmann::json& targetArgs = json["targetArgs"];
+		for (const auto& args : targetArgs.items())
 		{
-			p->configArgs[args.key()] = args.value();
+			if (p->configArgs.find(args.key()) == p->configArgs.end())
+				p->configArgs[args.key()] = args.value();
 		}
 	}
 
-	if (json.contains("autoBuild"))
-	{
-		p->autoBuild = json["autoBuild"];
-	}
+	tryGetField("autoBuild", p->autoBuild);
 }
